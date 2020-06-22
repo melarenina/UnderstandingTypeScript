9bab962823cf83bfc2b33a8f0483b1b4c7e4f450
Melare Marina (CI/DAV3.8) <mml8ca@bosch.com>
2020-06-22 08:39:05 -0300

    Tuples

--- app.ts
+++ app.ts
@@ -2,11 +2,17 @@
 //   name: string; // What variables and types this object must have
 //   age: number;
 // } = { 
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: 'Marina',
   age: 30,
-  hobbies: ['Sports', 'Cooking']
-}; 
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'] // It must always have two elements, where the first is a number, and the second is a string
+};
 
 // let favoriteActivities: any[];
 // favoriteActivities = ['Sports', 1];
