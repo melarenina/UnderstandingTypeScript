40c09f308fda5e0fe3fe55b5cc234bf74d330e4d
Melare Marina (CI/DAV3.8) <mml8ca@bosch.com>
2020-06-22 12:01:08 -0300

    Function Types & Callbacks

--- app.ts
+++ app.ts
@@ -6,6 +6,11 @@ function printResult(num: number) { // a void function
   console.log('Result: ' + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(5, 12));
 
 let combineValues: (a: number, b: number) => number; // Function type - returns a number and take 2 number arguments
@@ -13,5 +18,9 @@ let combineValues: (a: number, b: number) => number; // Function type - returns 
 combineValues = add;
 
 console.log(combineValues(8, 8))
+
+addAndHandle(10, 20, (result) => { 
+  console.log(result);
+});
 
 // let someValue: undefined;
