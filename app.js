ea48908252dee3a543a167581ca7a42dd2dca92a
Melare Marina (CI/DAV3.8) <mml8ca@bosch.com>
2020-06-22 08:14:38 -0300

    Object Types

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
-function add(n1, n2, phrase, showResult) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return n1 + n2;
-    }
-    // if (typeof n1 !== 'number' && typeof n2 !== 'number') {
-    //   throw new Error('Incorret input!');
-    // }
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'The Result Is: ';
-var result = add(number1, number2, resultPhrase, printResult);
-console.log(result);
+var person = {
+    name: 'Marina',
+    age: 30
+};
+console.log(person);
--- app.ts
+++ app.ts
@@ -1,20 +1,10 @@
-function add(n1: number, n2: number, phrase: string, showResult: boolean) {
-  const result = n1 + n2
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return n1 + n2;
-  }
+// const person: { // {} TypeScript notation that is an object type
+//   name: string; // What variables and types this object must have
+//   age: number;
+// } = { 
+const person = {
+  name: 'Marina',
+  age: 30
+}; 
 
-    // if (typeof n1 !== 'number' && typeof n2 !== 'number') {
-    //   throw new Error('Incorret input!');
-    // }
-}
-
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'The Result Is: ';
-
-const result = add(number1, number2, resultPhrase, printResult);
-console.log(result);
+console.log(person.name);
