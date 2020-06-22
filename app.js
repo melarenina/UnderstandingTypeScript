7ffd6666abfc0521b01c7a928b093595c5b5d88c
Melare Marina (CI/DAV3.8) <mml8ca@bosch.com>
2020-06-22 07:49:20 -0300

    Type Assignment & Type Inference

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-function add(n1, n2) {
-    console.log(typeof number1);
-    return n1 + n2;
+function add(n1, n2, phrase, showResult) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return n1 + n2;
+    }
+    // if (typeof n1 !== 'number' && typeof n2 !== 'number') {
+    //   throw new Error('Incorret input!');
+    // }
 }
 var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
+var printResult = true;
+var resultPhrase = 'The Result Is: ';
+var result = add(number1, number2, resultPhrase, printResult);
 console.log(result);
