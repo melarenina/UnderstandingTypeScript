0a9deec5dcdea09b5e1f950e4322420e7f0830ee
Melare Marina (CI/DAV3.8) <mml8ca@bosch.com>
2020-06-22 08:31:43 -0300

    Arrays Types

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+// const person: { // {} TypeScript notation that is an object type
+//   name: string; // What variables and types this object must have
+//   age: number;
+// } = { 
 var person = {
     name: 'Marina',
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
 };
-console.log(person);
+// let favoriteActivities: any[];
+// favoriteActivities = ['Sports', 1];
+var favoriteActivities;
+favoriteActivities = ['Sports'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -4,7 +4,18 @@
 // } = { 
 const person = {
   name: 'Marina',
-  age: 30
+  age: 30,
+  hobbies: ['Sports', 'Cooking']
 }; 
 
+// let favoriteActivities: any[];
+// favoriteActivities = ['Sports', 1];
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
